7573e9c5d106e8186b64074e66d685394f8ff501
Zak Lantz <zakodewald@gmail.com>
2020-07-28 15:11:13 -0400

    Better logging, extra exception handling

--- bytesized-api.py
+++ bytesized-api.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+from datetime import datetime
 import sys
 import argparse
 from prometheus_client import start_http_server, Gauge
@@ -36,6 +37,7 @@ def get_byte_stats():
     memory_usage = response_json[0]['memory_usage']
 
     if response.status_code == 200:
+        sys.stdout.write(str(datetime.now()) + '\n')
         sys.stdout.write(str(response) + '\n')
         sys.stdout.write('SERVER_NAME: ' + server_name + '\n')
         sys.stdout.write('MEMORY_USAGE: ' + str(memory_usage) + '\n')
@@ -47,14 +49,20 @@ def get_byte_stats():
     
     else:
         sys.stderr.write('Request failed ' + str(response) + '\n\n')
+        exit()
 
 if __name__ == '__main__':
-    start_http_server(8888, addr='0.0.0.0')
-    while True:
-        try:
-            get_byte_stats()
-        except:
-            sys.stderr.write('Request to api failed.\nPlease verify you have the correct api key.')
-            exit()
-        time.sleep(interval)
+    try:
+        start_http_server(8888, addr='0.0.0.0')
+        sys.stdout.write('WEBSERVER STARTED SUCCESSFULLY: 0.0.0.0:8888\n\n')
+        while True:
+            try:
+                get_byte_stats()
+            except:
+                sys.stderr.write('Request to api failed.\nPlease verify you have the correct api key.')
+                exit()
+            time.sleep(interval)
 
+    except:
+        sys.stderr.write('Webserver failed to start. Verify port is not in use')
+        exit()
